50e8e130cc55aae4f63f3e07f562e2fe6f37f074
aklilu <akliludin@gmail.com>
2020-04-18 00:49:03 +0200

    Update Geo_settings.R

--- scripts/Geo_settings.R
+++ scripts/Geo_settings.R
@@ -5,14 +5,15 @@ country_settings <- list(
   "uganda" = list( ),
   "Ethiopia" = list("NMA_stations"="geonode:nam_stations",
                     "eth_hydro_st"="geonode:eth_hydro_st",
-					"impact_admin3"="geonode:eth_impact_data2",
+		    "impact_data"="geonode:eth_impact_data2",
                     "admin2"="geonode:eth_admin2_2019",
                     "admin1"="geonode:eth_admin1_2019",
                     "admin3"="geonode:eth_admin3_2019"),
   "general_geo" = list( "glofas_st"="geonode:glofas_stations_africa",  
                     "basins_africa"="geonode:hybas_af_lev12_v1", 
-                    "rivers"="geonode:af_riv_15s"),
-   "kenya" = list( "admin2"="geonode:ken_adm2_mapshaper",
+                    "rivers"="geonode:af_riv_15s"), 
+   "kenya" = list( "impact_data"="geonode:geolocation_of_impact",
+		    "admin2"="geonode:ken_adm2_mapshaper",
                     "admin1"="geonode:ken_adm1_mapshaper",
                     "admin3"="geonode:ken_adm3_mapshaper"  )
 )
